319067e2e2962fa59609be3fb7bdc6f0bcc772dc
Andrew Thompson <andrewthompson@dhcp-172-20-180-92.dhcp.thefacebook.com>
2017-07-06 15:51:01 -0700

    correct script for install

--- install_docker_1.sh
+++ install_docker_1.sh
@@ -5,3 +5,18 @@ sudo apt-get install \
     ca-certificates \
     curl \
     software-properties-common
+
+
+echo "Adding Docker’s official GPG key: "
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+
+
+echo "updating"
+
+sudo apt-get update
+
+
+echo "install ce"
+
+sudo apt-get install docker-ce
