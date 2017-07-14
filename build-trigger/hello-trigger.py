7965840fc00956b71c447db899c813d33247aa0c
Andrew S Thompson <admin@entercloud.info>
2017-07-13 20:56:24 -0700

    ec_ui artifacts

--- build-trigger/hello-trigger.py
+++ build-trigger/hello-trigger.py
@@ -1,9 +1,9 @@
  
 import jenkins 
 
-def lambda_handler(event, context):
+
   server = jenkins.Jenkins('http://52.53.188.33:8080', username='andrewthompson', password='admin')
-  user = server.get_whoami()
+  user = serer.get_whoami()
   target_build = server.build_job('post-action-trigger-build-job')
  
-  return  user 
+  print ("current user is at:" user) 
--- build-trigger/nodes/ec_ui.py
+++ build-trigger/nodes/ec_ui.py
@@ -1,5 +1,5 @@
 from urllib import * 
 
 url_request = urlopen('http://entercloud.info/index_ccc.html')
-ec_ui = url_request()
+ec_ui = url_request.read()
 
